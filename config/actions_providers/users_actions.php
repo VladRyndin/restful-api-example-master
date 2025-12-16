f50431ddcdfb6327a299e47da0b1c258068c1b33
VladRyndin <58593480+VladRyndin@users.noreply.github.com>
2025-12-16 21:10:31 +0200

    add RefreshAction binding and reorder user action bindings

--- config/actions_providers/users_actions.php
+++ config/actions_providers/users_actions.php
@@ -1,8 +1,9 @@
 <?php
 
 return [
-    \App\Contracts\Actions\User\RegistrationActionContract::class => \App\Actions\User\Auth\RegistrationAction::class,
     \App\Contracts\Actions\User\LoginActionContract::class => \App\Actions\User\Auth\LoginAction::class,
-    \App\Contracts\Actions\User\UserDataActionContract::class => \App\Actions\User\Auth\UserDataAction::class,
     \App\Contracts\Actions\User\LogoutActionContract::class => \App\Actions\User\Auth\LogoutAction::class,
+    \App\Contracts\Actions\User\RefreshActionContract::class => \App\Actions\User\Auth\RefreshAction::class,
+    \App\Contracts\Actions\User\UserDataActionContract::class => \App\Actions\User\Auth\UserDataAction::class,
+    \App\Contracts\Actions\User\RegistrationActionContract::class => \App\Actions\User\Auth\RegistrationAction::class,
 ];
